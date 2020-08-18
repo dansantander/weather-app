0d27957e2aca9371cf86d9aca7c64ed84d50e1a8
Daniel Santander <danyvo1d@gmail.com>
2020-08-18 16:58:13 -0500

    Set searchbar functionality

--- src/api.js
+++ src/api.js
@@ -1,22 +1,22 @@
 import Weather from './classes/weather';
 
 const getWeather = (() => {
-  const weatherQuery = () => new Promise((resolve, reject) => {
-    fetch('http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=d5c069562c9fcd7849eca2747d7a7982',
-      { mode: 'cors' }).then(response => response.json()).then((response) => {
-      if (response) {
-        console.log(response);
-        const weather = new Weather(response.name, response.sys.country,
-          response.weather[0].description, response.wind.speed, response.main.humidity,
-          response.main.temp, response.weather[0].icon);
-        console.log(weather);
-        resolve(weather);
-      } else {
-        reject();
-      }
-    });
+  const weatherQuery = (city) => new Promise((resolve, reject) => {
+    const base = 'https://api.openweathermap.org/data/2.5/weather';
+    const key = 'd5c069562c9fcd7849eca2747d7a7982';
+    fetch(`${base}?q=${city}&units=imperial&APPID=${key}`, { mode: 'cors' })
+      .then(response => response.json()).then((response) => {
+        if (response) {
+          const weather = new Weather(response.name, response.sys.country,
+            response.weather[0].description, response.wind.speed, response.main.humidity,
+            response.main.temp, response.weather[0].icon);
+          resolve(weather);
+        } else {
+          reject();
+        }
+      });
   });
-  return { weatherQuery  };
+  return { weatherQuery };
 })();
 
 export default getWeather;
--- src/interface.js
+++ src/interface.js
@@ -1,17 +1,52 @@
 import Api from './api';
 
 const appInterface = (() => {
+  const farenheitSelector = document.querySelector('.farenheit');
+  const celsiusSelector = document.querySelector('.celsius');
+  const city = document.querySelector('.city');
+  const country = document.querySelector('.country');
+  const tempValue = document.querySelector('.temp-value');
+  const tempDescription = document.querySelector('.temp-description p');
+  const windValue = document.querySelector('.wind');
+  const humidityValue = document.querySelector('.humidity');
+  const iconValue = document.getElementById('icon');
+  const inputValue = document.getElementById('city-input');
+  const errorMessage = document.getElementById('err-message');
+
   const renderHTML = (response) => {
-    const city = document.querySelector('.city');
-    const tempValue = document.querySelector('.temp-value');
-    const tempDescription = document.querySelector('.temp-description p');
     city.innerHTML = response.city;
-    tempValue.innerHTML = response.temperature;
+    country.innerHTML = response.country;
+    tempValue.innerHTML = Math.round(response.temperature);
     tempDescription.innerHTML = response.expected;
+    windValue.innerHTML = response.wind;
+    humidityValue.innerHTML = response.humidity;
+    iconValue.src = `http://openweathermap.org/img/wn/${response.icon}@2x.png`;
   };
 
-  Api.weatherQuery().then((response) => {
-    renderHTML(response);
+  const switchToFarenheit = (celsiusDegrees) => {
+    const farenheitResult = Math.round((celsiusDegrees * (9 / 5)) + 32);
+    return farenheitResult;
+  };
+
+  inputValue.addEventListener('keypress', (e) => {
+    if (e.code === 'Enter' || e.code === 'NumpadEnter') {
+      if (inputValue.value.trim() === '') {
+        errorMessage.classList.toggle('d-none');
+      } else {
+        errorMessage.classList.toggle('d-none');
+        Api.weatherQuery(inputValue.value).then((response) => {
+          renderHTML(response);
+
+          farenheitSelector.addEventListener('click', () => {
+            tempValue.innerHTML = switchToFarenheit(response.temperature);
+          });
+
+          celsiusSelector.addEventListener('click', () => {
+            tempValue.innerHTML = Math.round(response.temperature);
+          });
+        });
+      }
+    }
   });
 })();
 
