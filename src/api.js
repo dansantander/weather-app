38463c95691554f3a52b1f1ef7553dbab56234ff
Daniel Santander <danyvo1d@gmail.com>
2020-08-18 10:19:01 -0500

    Fixing fetch

--- src/api.js
+++ src/api.js
@@ -1,23 +1,41 @@
 import Weather from './classes/weather';
 // api.openweathermap.org/data/2.5/weather?q={city name}&appid={your api key}
 
-/* const getWeather = async (city, units, action) => {
-  const base = 'https://api.openweathermap.org/data/2.5/weather';
-  const key = 'd5c069562c9fcd7849eca2747d7a7982';
-  const response = await fetch(`${base}?q=${city()}&APPID=${key}&units=${units()}`,
-   { mode: 'cors' });
-  saveData(await response.json());
-  action();
-}; */
-
-const getWeather = () => {
+/* const getWeather = () => {
   fetch('http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=d5c069562c9fcd7849eca2747d7a7982', { mode: 'cors' })
-    .then(function(response) {
+    .then((response) => {
       return response.json();
     })
-    .then(function(response) {
+    .then((response) => {
       console.log(response);
     });
+};
+ */
+
+const getWeather = () => {
+  console.log('Working?');
+  fetch('http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=d5c069562c9fcd7849eca2747d7a7982', { mode: 'cors' })
+    .then((response) => response.json().then((response) => {
+      console.log('Working inside then?');
+      if (response) {
+        const weather = new Weather(response.name, response.sys.country,
+          response.weather[0].description, response.wind.speed, response.main.humidity,
+          response.main.temp, response.weather[0].icon);
+/* 
+          const title = document.querySelector('.title');
+          console.log(title);
+          console.log(weather);
+
+          title.innerHTML = weather.city;
+           */
+            console.log(weather);
+            
+            const res = Promise.resolve(weather);
+           return res;
+      
+      }
+      console.log('Error');
+    }));
 };
 
 export default getWeather;
--- src/interface.js
+++ src/interface.js
@@ -1,18 +1,13 @@
-import weather from './classes/weather';
 import getWeather from './api';
 
 const appInterface = (() => {
-  const title = document.querySelector('.title');
-  const temperature = document.querySelector('.temp-value');
-  const description = document.querySelector('.temp-description');
-  const city = document.querySelector('.city');
-  const country = document.querySelector('.country');
-
-  title.innerHTML = getWeather();
-  temperature.innerHTML = `${weather.temperature}`;
-  description.innerHTML = `${weather.description}`;
-  city.innerHTML = `${weather.city}`;
-  country.innerHTML = `${weather.country}`;
+  console.log('In here?');
+  /* const title = document.querySelector('.title'); */
+  getWeather().then((response) => {
+    console.log('Anything');
+    const varResponse = response;
+    console.log(varResponse);
+  });
 })();
 
 export default appInterface;
